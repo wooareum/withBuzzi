f7d57446b6d75f083f8e1c40218f0c6b67e99c2f
wooareum <bezzl1122@gmail.com>
2018-03-09 22:12:58 +0900

    test
    test

---
+++ testWithBuzzi.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main (void) {
+    int n = 99;
+    for(int i = 0 ; i < n; i++) {
+      if(i/2==1 || i/2==0)
+        printf("oppa babo\n");
+      else
+        printf("Areum good\n");
+    }
+}
